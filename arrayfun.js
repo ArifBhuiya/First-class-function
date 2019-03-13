640b3e049d69abfc2c4160261d63b9fac2aab057
Arif Bhuiya <arifulislam517@gmail.com>
2019-03-13 17:56:56 +0300

    create an array function

---
+++ arrayfun.js
@@ -0,0 +1,28 @@
+//var arr =[7,9,0,1,2,4,5,6,7,];
+
+
+// arr.forEach(function(element,index ,arr){
+//     console.log('The Elements is = '+ element+ ' index is = '+ index + ' arr is = ' + arr );
+
+// });
+
+// function myfun(arr){
+//   for(var i = 0; i<arr.length;  i++)
+//     console.log(arr[i]);
+
+// }
+// myfun(arr);
+
+
+
+// function loop(arr,callback){
+//   for(var i=0; i<arr.length;i++){
+//     callback(arr[i],i);
+//   }
+    
+
+// }
+// loop(arr,function(element,i){
+//      console.log('Element is = ' + element +' index is = '+ i);
+     
+// });
